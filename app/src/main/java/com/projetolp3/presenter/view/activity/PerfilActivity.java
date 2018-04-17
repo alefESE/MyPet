00dd10968c62c756fcb8ef9e097b6ee033e87aa0
ednaldo <martinsednaldo@outlook.com>
2018-04-17 16:12:15 -0300

    Atualização da tela de perfil. TableRow foi apagado e utilizou-se apenas ConstraintLayout.

--- app/src/main/java/com/projetolp3/presenter/view/activity/PerfilActivity.java
+++ app/src/main/java/com/projetolp3/presenter/view/activity/PerfilActivity.java
@@ -1,7 +1,13 @@
 package com.projetolp3.presenter.view.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.projetolp3.mypet.R;
 
@@ -11,5 +17,7 @@ public class PerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
     }
+
 }
